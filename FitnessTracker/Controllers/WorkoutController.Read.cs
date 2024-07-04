f885ed497fe0b16f174b6cb584a46194cf136c7a
Andrej Nenadić <andrejnana445@gmail.com>
2024-07-05 00:03:55 +0200

    Added offset and limit to workout get endpoints in query params (pagination) and created an endpoint for getting favorite workouts

--- FitnessTracker/Controllers/WorkoutController.Read.cs
+++ FitnessTracker/Controllers/WorkoutController.Read.cs
@@ -11,22 +11,22 @@ namespace FitnessTracker.Controllers
     {
         [HttpGet("public/simple")]
         [ProducesResponseType(typeof(IEnumerable<SimpleWorkoutResponseDTO>), StatusCodes.Status200OK)]
-        public async Task<IActionResult> GetAllSimplePublic([FromQuery] string? name)
+        public async Task<IActionResult> GetAllSimplePublic([FromQuery] string? name, [FromQuery] int? limit, [FromQuery] int? offset)
         {
             var workouts = name is null
-                ? await readRangeService.Get(x => x.IsPublic, 0, 10, "creator")
-                : await readRangeService.Get(x => x.IsPublic && EF.Functions.Like(x.Name, $"%{name}%"), 0, 10, "creator");
+                ? await readRangeService.Get(x => x.IsPublic, offset, limit ?? 10, "creator")
+                : await readRangeService.Get(x => x.IsPublic && EF.Functions.Like(x.Name, $"%{name}%"), offset, limit ?? 10, "creator");
 
             return Ok(workouts.Select(simpleResponseMapper.Map));
         }
 
         [HttpGet("public/simple/by/{userId}")]
         [ProducesResponseType(typeof(IEnumerable<SimpleWorkoutResponseDTO>), StatusCodes.Status200OK)]
-        public async Task<IActionResult> GetAllSimplePublic(Guid userId, [FromQuery] string? name)
+        public async Task<IActionResult> GetAllSimplePublic(Guid userId, [FromQuery] string? name, [FromQuery] int? limit, [FromQuery] int? offset)
         {
             var workouts = name is null
-                ? await readRangeService.Get(x => x.CreatorId == userId && x.IsPublic, 0, 10, "creator")
-                : await readRangeService.Get(x => x.CreatorId == userId && x.IsPublic && EF.Functions.Like(x.Name, $"%{name}%"), 0, 10, "creator");
+                ? await readRangeService.Get(x => x.CreatorId == userId && x.IsPublic, offset, limit ?? 10, "creator")
+                : await readRangeService.Get(x => x.CreatorId == userId && x.IsPublic && EF.Functions.Like(x.Name, $"%{name}%"), offset, limit ?? 10, "creator");
 
             return Ok(workouts.Select(simpleResponseMapper.Map));
         }
@@ -36,7 +36,7 @@ namespace FitnessTracker.Controllers
         [ProducesResponseType(typeof(IEnumerable<SimpleWorkoutResponseDTO>), StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         [ProducesResponseType(StatusCodes.Status403Forbidden)]
-        public async Task<IActionResult> GetAllSimplePersonal([FromQuery] string? name)
+        public async Task<IActionResult> GetAllSimplePersonal([FromQuery] string? name, [FromQuery] int? limit, [FromQuery] int? offset)
         {
             if (User.Identity is not ClaimsIdentity claimsIdentity
                 || claimsIdentity.FindFirst(ClaimTypes.NameIdentifier)?.Value is not string userIdString
@@ -44,8 +44,26 @@ namespace FitnessTracker.Controllers
                 return Unauthorized();
 
             var workouts = name is null
-                ? await readRangeService.Get(x => x.CreatorId == userId, 0, 10, "creator")
-                : await readRangeService.Get(x => x.CreatorId == userId && EF.Functions.Like(x.Name, $"%{name}%"), 0, 10, "creator");
+                ? await readRangeService.Get(x => x.CreatorId == userId, offset, limit ?? 10, "creator")
+                : await readRangeService.Get(x => x.CreatorId == userId && EF.Functions.Like(x.Name, $"%{name}%"), offset, limit ?? 10, "creator");
+
+            return Ok(workouts.Select(simpleResponseMapper.Map));
+        }
+
+        [Authorize]
+        [HttpGet("favorite/simple")]
+        [ProducesResponseType(typeof(IEnumerable<SimpleWorkoutResponseDTO>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        public async Task<IActionResult> GetAllSimpleFavorites([FromQuery] string? name, [FromQuery] int? limit, [FromQuery] int? offset)
+        {
+            if (User.Identity is not ClaimsIdentity claimsIdentity
+                || claimsIdentity.FindFirst(ClaimTypes.NameIdentifier)?.Value is not string userIdString
+                || !Guid.TryParse(userIdString, out var userId))
+                return Unauthorized();
+
+            var workouts = name is null
+                ? await readRangeService.Get(x => (x.IsPublic || x.CreatorId == userId) && x.Favorites.Any(x => x.Id == userId), offset, limit ?? 10, "creator,favorites")
+                : await readRangeService.Get(x => (x.IsPublic || x.CreatorId == userId) && x.Favorites.Any(x => x.Id == userId) && EF.Functions.Like(x.Name, $"%{name}%"), offset, limit ?? 10, "creator,favorites");
 
             return Ok(workouts.Select(simpleResponseMapper.Map));
         }
