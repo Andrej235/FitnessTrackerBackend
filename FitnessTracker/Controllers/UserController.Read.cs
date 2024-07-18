cc50097b81fa0a761b4c8cb563dcb2ff9efd976a
Andrej Nenadić <andrejnana445@gmail.com>
2024-07-18 22:59:32 +0200

    Added endpoints for getting current users followers (/me) and added filtering by name for following / followers on both /user/{id} and /user/me

--- FitnessTracker/Controllers/UserController.Read.cs
+++ FitnessTracker/Controllers/UserController.Read.cs
@@ -2,6 +2,7 @@
 using FitnessTracker.Utilities;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Security.Claims;
 
 namespace FitnessTracker.Controllers
@@ -20,10 +21,7 @@ namespace FitnessTracker.Controllers
                 return Unauthorized();
 
             var user = await readSingleService.Get(x => x.Id == userId, "detailed");
-            if (user is null)
-                return Unauthorized();
-
-            return Ok(detailedResponseMapper.Map(user));
+            return user is null ? Unauthorized() : Ok(detailedResponseMapper.Map(user));
         }
 
         [HttpGet("{id}/detailed")]
@@ -52,11 +50,14 @@ namespace FitnessTracker.Controllers
         [HttpGet("{id}/following")]
         [ProducesResponseType(typeof(IEnumerable<SimpleUserResponseDTO>), StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public async Task<IActionResult> GetFollowing(Guid id, [FromQuery] int? limit, [FromQuery] int? offset)
+        public async Task<IActionResult> GetFollowing(Guid id, [FromQuery] string? name, [FromQuery] int? limit, [FromQuery] int? offset)
         {
             try
             {
-                var follows = await followerReadRangeService.Get(x => x.FollowerId == id, offset ?? 0, limit ?? 10, "followee");
+                var follows = name is null
+                    ? await followerReadRangeService.Get(x => x.FollowerId == id, offset ?? 0, limit ?? 10, "followee")
+                    : await followerReadRangeService.Get(x => x.FollowerId == id && EF.Functions.Like(x.Followee.Name, $"{name}%"), offset ?? 0, limit ?? 10, "followee");
+
                 return Ok(follows.Select(x => simpleResponseMapper.Map(x.Followee)));
             }
             catch (Exception ex)
@@ -69,11 +70,68 @@ namespace FitnessTracker.Controllers
         [HttpGet("{id}/followers")]
         [ProducesResponseType(typeof(IEnumerable<SimpleUserResponseDTO>), StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public async Task<IActionResult> GetFollowers(Guid id, [FromQuery] int? limit, [FromQuery] int? offset)
+        public async Task<IActionResult> GetFollowers(Guid id, [FromQuery] string? name, [FromQuery] int? limit, [FromQuery] int? offset)
         {
             try
             {
-                var follows = await followerReadRangeService.Get(x => x.FolloweeId == id, offset ?? 0, limit ?? 10, "follower");
+                var follows = name is null
+                    ? await followerReadRangeService.Get(x => x.FolloweeId == id, offset ?? 0, limit ?? 10, "follower")
+                    : await followerReadRangeService.Get(x => x.FolloweeId == id && EF.Functions.Like(x.Follower.Name, $"{name}%"), offset ?? 0, limit ?? 10, "follower");
+
+                return Ok(follows.Select(x => simpleResponseMapper.Map(x.Follower)));
+            }
+            catch (Exception ex)
+            {
+                ex.LogError();
+                return NotFound();
+            }
+        }
+
+        [Authorize]
+        [HttpGet("me/following")]
+        [ProducesResponseType(typeof(IEnumerable<SimpleUserResponseDTO>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> GetFollowing([FromQuery] string? name, [FromQuery] int? limit, [FromQuery] int? offset)
+        {
+            try
+            {
+                if (User.Identity is not ClaimsIdentity claimsIdentity
+                    || claimsIdentity.FindFirst(ClaimTypes.NameIdentifier)?.Value is not string userIdString
+                    || !Guid.TryParse(userIdString, out var id))
+                    return Unauthorized();
+
+                var follows = name is null
+                    ? await followerReadRangeService.Get(x => x.FollowerId == id, offset ?? 0, limit ?? 10, "followee")
+                    : await followerReadRangeService.Get(x => x.FollowerId == id && EF.Functions.Like(x.Followee.Name, $"{name}%"), offset ?? 0, limit ?? 10, "followee");
+
+                return Ok(follows.Select(x => simpleResponseMapper.Map(x.Followee)));
+            }
+            catch (Exception ex)
+            {
+                ex.LogError();
+                return NotFound();
+            }
+        }
+
+        [Authorize]
+        [HttpGet("me/followers")]
+        [ProducesResponseType(typeof(IEnumerable<SimpleUserResponseDTO>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> GetFollowers([FromQuery] string? name, [FromQuery] int? limit, [FromQuery] int? offset)
+        {
+            try
+            {
+                if (User.Identity is not ClaimsIdentity claimsIdentity
+                    || claimsIdentity.FindFirst(ClaimTypes.NameIdentifier)?.Value is not string userIdString
+                    || !Guid.TryParse(userIdString, out var id))
+                    return Unauthorized();
+
+                var follows = name is null
+                    ? await followerReadRangeService.Get(x => x.FolloweeId == id, offset ?? 0, limit ?? 10, "follower")
+                    : await followerReadRangeService.Get(x => x.FolloweeId == id && EF.Functions.Like(x.Follower.Name, $"{name}%"), offset ?? 0, limit ?? 10, "follower");
+
                 return Ok(follows.Select(x => simpleResponseMapper.Map(x.Follower)));
             }
             catch (Exception ex)
