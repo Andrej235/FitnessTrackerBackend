ea321d5b767458dddd3c89718a9f959633e1f7c3
Andrej Nenadić <andrejnana445@gmail.com>
2024-07-19 11:46:34 +0200

    Added an endpoint for deleting equipment

--- FitnessTracker/Controllers/EquipmentController.cs
+++ FitnessTracker/Controllers/EquipmentController.cs
@@ -2,6 +2,7 @@
 using FitnessTracker.DTOs.Responses.Equipment;
 using FitnessTracker.Models;
 using FitnessTracker.Services.Create;
+using FitnessTracker.Services.Delete;
 using FitnessTracker.Services.Mapping.Request;
 using FitnessTracker.Services.Mapping.Response;
 using FitnessTracker.Services.Read.ExpressionBased;
@@ -13,14 +14,16 @@ namespace FitnessTracker.Controllers
     [Route("api/equipment")]
     [ProducesResponseType(StatusCodes.Status429TooManyRequests)]
     public partial class EquipmentController(ICreateService<Equipment> createService,
-                                     ICreateRangeService<Equipment> createRangeService,
-                                     IReadRangeService<Equipment> readRangeService,
-                                     IRequestMapper<CreateEquipmentRequestDTO, Equipment> requestMapper,
-                                     IResponseMapper<Equipment, SimpleEquipmentResponseDTO> responseMapper) : ControllerBase
+                                             ICreateRangeService<Equipment> createRangeService,
+                                             IReadRangeService<Equipment> readRangeService,
+                                             IDeleteService<Equipment> deleteService,
+                                             IRequestMapper<CreateEquipmentRequestDTO, Equipment> requestMapper,
+                                             IResponseMapper<Equipment, SimpleEquipmentResponseDTO> responseMapper) : ControllerBase
     {
         private readonly ICreateService<Equipment> createService = createService;
         private readonly ICreateRangeService<Equipment> createRangeService = createRangeService;
         private readonly IReadRangeService<Equipment> readRangeService = readRangeService;
+        private readonly IDeleteService<Equipment> deleteService = deleteService;
         private readonly IRequestMapper<CreateEquipmentRequestDTO, Equipment> requestMapper = requestMapper;
         private readonly IResponseMapper<Equipment, SimpleEquipmentResponseDTO> responseMapper = responseMapper;
     }
