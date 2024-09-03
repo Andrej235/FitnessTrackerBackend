4483ae017ad0d3bbfb73507f5506df3e00a38f51
Andrej Nenadić <andrejnana445@gmail.com>
2024-09-03 09:33:27 +0200

    Add documentation for IReadSingleService and IReadRangeService's methods

--- FitnessTracker/Services/Read/IReadRangeService.cs
+++ FitnessTracker/Services/Read/IReadRangeService.cs
@@ -4,6 +4,25 @@ namespace FitnessTracker.Services.Read
 {
     public interface IReadRangeService<T> where T : class
     {
+        /// <summary>
+        /// Finds all entities in the database which fit the provided criteria
+        /// </summary>
+        /// <param name="criteria">Criteria which is used to search for specific entities in the database</param>
+        /// <param name="offset">
+        /// Number of entities which will be skipped when creating the output list
+        /// <br/>If the value is 0, no entities will be skipped
+        /// <br/>If the value is null, no entities will be skipped
+        /// </param>
+        /// <param name="limit">
+        /// Maximum number of entities which will be included in the output list
+        /// <br/>If a negative value is entered, output will include all entities which fit the criteria
+        /// <br/>If the value is null, output will include all entities which fit the criteria
+        /// </param>
+        /// <param name="queryBuilder">
+        /// Used to further modify the query
+        /// It allows 4 methods: Include, ThenInclude, OrderBy and OrderByDescending
+        /// </param>
+        /// <returns>Return a list of entities that fit the provided criteria, if no such entity exists an empty list will be returned</returns>
         Task<IEnumerable<T>> Get(Expression<Func<T, bool>>? criteria, int? offset = 0, int? limit = -1, Func<IWrappedQueryable<T>, IWrappedResult<T>>? queryBuilder = null);
     }
 }
--- FitnessTracker/Services/Read/IReadSingleService.cs
+++ FitnessTracker/Services/Read/IReadSingleService.cs
@@ -4,6 +4,15 @@ namespace FitnessTracker.Services.Read
 {
     public interface IReadSingleService<T> where T : class
     {
+        /// <summary>
+        /// Finds a first entity in database which fits the provided criteria
+        /// </summary>
+        /// <param name="criteria">Criteria which is used to search for a specific entity in the database</param>
+        /// <param name="queryBuilder">
+        /// Used to further modify the query
+        /// It allows 4 methods: Include, ThenInclude, OrderBy and OrderByDescending
+        /// </param>
+        /// <returns>Returns a first entity that fits the provided criteria or if such entity doesn't exist, null</returns>
         Task<T?> Get(Expression<Func<T, bool>> criteria, Func<IWrappedQueryable<T>, IWrappedResult<T>>? queryBuilder = null);
     }
 }
