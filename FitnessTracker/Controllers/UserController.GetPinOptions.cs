05717ad9ba4450b5ef68331bf645a66257a8c5d4
Andrej Nenadić <andrejnana445@gmail.com>
2024-09-04 21:10:54 +0200

    Add an endpoint for reading pin options

---
+++ FitnessTracker/Controllers/UserController.GetPinOptions.cs
@@ -0,0 +1,47 @@
+﻿using FitnessTracker.DTOs.Responses.User;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace FitnessTracker.Controllers
+{
+    public partial class UserController
+    {
+        [Authorize]
+        [HttpGet("me/pins/options")]
+        [ProducesResponseType(typeof(IEnumerable<SimplePinOptionResponseDTO>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+
+        public async Task<IActionResult> GetPinOptions()
+        {
+            if (User.Identity is not ClaimsIdentity claimsIdentity
+                || claimsIdentity.FindFirst(ClaimTypes.NameIdentifier)?.Value is not string userIdString
+                || !Guid.TryParse(userIdString, out Guid userId))
+                return Unauthorized();
+
+            IEnumerable<SimplePinOptionResponseDTO>? pins = await readSingleSelectedService.Get(x =>
+                x.CreatedWorkouts.Where(x => x.IsPublic).Select(x => new SimplePinOptionResponseDTO
+                {
+                    Name = x.Name,
+                    LikeCount = x.Likes.Count,
+                    Type = SimplePinResponseDTO.PinType.Workout,
+                })
+                .Union(x.CreatedSplits.Where(x => x.IsPublic).Select(x => new SimplePinOptionResponseDTO
+                {
+                    Name = x.Name,
+                    LikeCount = x.Likes.Count,
+                    Type = SimplePinResponseDTO.PinType.Split,
+                }))
+                .OrderByDescending(x => x.LikeCount)
+                .ToList()
+            ,
+            x => x.Id == userId);
+
+            if (pins is null)
+                return NotFound();
+
+            return Ok(pins);
+        }
+    }
+}
---
+++ FitnessTracker/DTOs/Responses/User/SimplePinOptionResponseDTO.cs
@@ -0,0 +1,11 @@
+﻿using static FitnessTracker.DTOs.Responses.User.SimplePinResponseDTO;
+
+namespace FitnessTracker.DTOs.Responses.User
+{
+    public class SimplePinOptionResponseDTO
+    {
+        public string Name { get; set; } = null!;
+        public int LikeCount { get; set; }
+        public PinType Type { get; set; }
+    }
+}
